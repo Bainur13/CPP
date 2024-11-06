c7a5a74e0521e7078f13b4256af3ed14f9870d09
Bainur <urbain.dumas@gmail.com>
2024-11-06 17:11:48 +0100

    ex00 CPP07 finish

---
+++ CPP-07/ex00/main.cpp
@@ -0,0 +1,28 @@
+#include "whatever.hpp"
+
+int main()
+{
+    int a = 5;
+    int b = 67;
+
+    std::cout << "Before swapping a = " << a << " and b = " << b << std::endl;
+    swap(a, b);
+    std::cout << "After swapping a = " << a << " and b = " << b << std::endl;
+
+    std::cout << "The max is " << max(a, b) << std::endl;
+    std::cout << "The min is " << min(a, b) << std::endl << std::endl;
+    
+    a = 2;
+    b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    return 0;
+}
---
+++ CPP-07/ex01/iter.hpp
@@ -0,0 +1,14 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template <typename A, typename F>
+
+void iter(A &tab, int len, F func)
+{
+    for (int i; i < len; i++)
+    {
+        func(tab[i]);
+    }
+}
+
+#endif
